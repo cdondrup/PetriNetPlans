665be782678277b1d03f0d3550a1a3a0189b415f
Christian Dondrup <cdondrup@gmail.com>
2016-07-28 13:37:31 +0100

    Allowing to publish a string on `/planToExec` that contains the pnml xml in addition to being able to load it from a file.

--- PNPros/ROS_bridge/pnp_ros/src/ROSInst.cpp
+++ PNPros/ROS_bridge/pnp_ros/src/ROSInst.cpp
@@ -1,9 +1,10 @@
-#include <pnp_ros/ROSInst.h>
-#include <pnp_ros/ActionProxy.h>
-#include <pnp_ros/ROSConds.h>
-#include <pnp/basic_plan/xml_plan_instantiator.h>
-#include <pnp/pnp_plan.h>
+#include "pnp_ros/ROSInst.h"
+#include "pnp_ros/ActionProxy.h"
+#include "pnp_ros/ROSConds.h"
+#include "pnp/basic_plan/xml_plan_instantiator.h"
+#include "pnp/pnp_plan.h"
 #include <fstream>
+#include <iostream>
 
 using namespace PetriNetPlans;
 using namespace std;
@@ -18,45 +19,53 @@ namespace pnpros
 	{
 		string actionName, path;
 		ifstream file;
+
+        PnpPlan *plan = new PnpPlan(this,checker);
+        XMLPnpPlanInstantiator planLoader;
+
+        if ( name.find("<pnml>") != string::npos)
+        {
+            cout << "+++ Loading plan from string +++" << endl;
+            planLoader.loadFromString(name, plan);
+            return plan;
+        }
 		
-		unsigned long index = name.find('#');
+        unsigned long index = name.find('#');
 		
 		actionName = name;
 		
-		if (index != string::npos)
-		{
-			// Removing robot information to check if the action is a sub-plan.
-			actionName = actionName.substr(index + 1);
-		}
+        if (index != string::npos)
+        {
+            // Removing robot information to check if the action is a sub-plan.
+            actionName = actionName.substr(index + 1);
+        }
 		
 		path = planFolder + "/" + actionName + ".pnml";
 		
 		file.open(path.c_str(),ifstream::in);
 		file.close();
-		
-		if (file.fail())
+
+        if (file.fail())
 		{
-			file.clear(ios::failbit);
-			// cout << "No plan found. Trying with an action ..." << endl;
+
+            file.clear(ios::failbit);
+            cout << "No plan found. Trying with an action ..." << endl;
 			
-			return new ActionProxy(name);
+            return new ActionProxy(name);
 		}
 		
-		PnpPlan *plan = new PnpPlan(this,checker);
-		
+
 		try
-		{
-			XMLPnpPlanInstantiator planLoader;
-			
+		{			
 			planLoader.loadFromPNML(path,plan);
 		}
 		catch(const runtime_error&)
 		{
 			string errorString("No action nor plan with name: ");
 			
-			errorString += actionName;
-			
-			throw runtime_error(errorString);
+            errorString += actionName;
+
+            throw runtime_error(errorString);
 		}
 		
 		return plan;
